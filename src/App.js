ebdf2200e07b5e80ef1b571308db16ab7f135349
khushmanvar <khushmanvar@gmail.com>
2021-12-24 22:02:08 +0530

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div class="rounded-lg shadow bg-base-200 drawer h-screen">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" /> 
+        <div class="flex flex-col items-center justify-center drawer-content">
+          <label for="my-drawer" class="btn btn-primary drawer-button">Open</label>
+        </div> 
+        <div class="drawer-side">
+          <label for="my-drawer" class="drawer-overlay"></label> 
+          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li>
+              <a>Menu Item</a>
+            </li> 
+            <li>
+              <a>Menu Item</a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-export default App;
+    </div>
+  )
+}
