76f8187a22c18756d7bdcf38850c7d0b2dc02aec
Manzee1609 <mansikalaria16@gmail.com>
2022-01-08 17:02:44 +0530

    cors errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react'
+import SignUp from './Signup';
 
-export default function App() {
-  return (
-    <div>
-      <div class="rounded-lg shadow bg-base-200 drawer h-screen">
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" /> 
-        <div class="flex flex-col items-center justify-center drawer-content">
-          <label for="my-drawer" class="btn btn-primary drawer-button">Open</label>
-        </div> 
-        <div class="drawer-side">
-          <label for="my-drawer" class="drawer-overlay"></label> 
-          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-            <li>
-              <a>Menu Item</a>
-            </li> 
-            <li>
-              <a>Menu Item</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-    </div>
-  )
+class App extends React.Component {
+  render(){
+    return (
+      <>
+      <SignUp/>
+      
+      </>
+    );
+  }
 }
+
+export default App;
